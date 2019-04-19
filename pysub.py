16eb0b2c800bb7c39f6f16a3e63ebee70108aebd
piotrs112 <piotrsocha01@gmail.com>
2019-04-19 13:59:51 +0200

    Working show lookup

--- pysub.py
+++ pysub.py
@@ -2,15 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 #from user_agent import generate_user_agent
 
-#headers = generate_user_agent(os=('win', 'mac', 'linux'))
-r = requests.get("https://addic7ed.com") #, headers=headers)
-if r.status_code != 200:
-    raise ConnectionError
+q_name = "Chilling Adventures of Sabrina"
+
+
+def connect(addr: str) -> requests.models.Response:
+    #headers = generate_user_agent(os=('win', 'mac', 'linux'))
+    r = requests.get(addr) #, headers=headers)
+    if r.status_code != 200:
+        raise ConnectionError
+    return r
+
+r = connect("https://addic7ed.com")
 
 soup = BeautifulSoup(r.text, "lxml")
 body = soup.body
 showlist = body.find('select', {'id':'qsShow'})
-shows = showlist('option')
+show = showlist.find(string=q_name).parent
 
-for show in shows [:10]:
+if show != None and show != []:
     print("NAME: {} ID: {}".format(show.get_text(), show.get('value')))
