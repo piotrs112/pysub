8311e8797c0831b1473415d5eb89d9be7036a1a0
piotrs112 <piotrsocha01@gmail.com>
2019-04-19 13:27:35 +0200

    Working tv shows list download

--- pysub.py
+++ pysub.py
@@ -4,4 +4,13 @@ from bs4 import BeautifulSoup
 
 #headers = generate_user_agent(os=('win', 'mac', 'linux'))
 r = requests.get("https://addic7ed.com") #, headers=headers)
-if(r.status_code !=  200) raise ConnectionError
+if r.status_code != 200:
+    raise ConnectionError
+
+soup = BeautifulSoup(r.text, "lxml")
+body = soup.body
+showlist = body.find('select', {'id':'qsShow'})
+shows = showlist('option')
+
+for show in shows [:10]:
+    print("NAME: {} ID: {}".format(show.get_text(), show.get('value')))
